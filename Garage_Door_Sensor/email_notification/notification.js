71a0be1666b3480783de2d2ce9fffa5bec30d14e
spatel9603 <145863533+spatel9603@users.noreply.github.com>
2024-11-07 00:59:21 -0600

    Comments Added and Added Message Formatting

--- Garage_Door_Sensor/email_notification/notification.js
+++ Garage_Door_Sensor/email_notification/notification.js
@@ -1,7 +1,10 @@
-const { initializeApp } = require('firebase/app');
+// Import Firebase Initialization, Firebase Commands, and Node Mailer
+const { initializeApp } = require('firebase/app'); 
 const { getDatabase, ref, onValue, set } = require('firebase/database');
 const nodemailer = require('nodemailer');
 
+
+// Conigure Firebase with Key
 const firebaseConfig = {
     apiKey: "AIzaSyBwOJ2HDW0Au-BpXfcX7Ii6EXwGKJ5GFhg",
     authDomain: "garage-door-sensor.firebaseapp.com",
@@ -13,9 +16,68 @@ const firebaseConfig = {
     measurementId: "G-HHCBBP5SB0"
   };
 
+// Initialize App
 const app = initializeApp(firebaseConfig);
+
+// Get Reference to Database
 const database = getDatabase(app);
 
+// Message for Email
+let message;
+
+/////////////////////////////////////////////////
+// Function: formatCurrentDateTime()
+// Description: This function gets the current 
+//              time and date and formats the 
+//              data into a message for the 
+//              email.
+/////////////////////////////////////////////////
+function messageTime() {
+
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+
+    // Check if PM
+    let PM = (hours >= 12);
+
+    //let hoursUpdated = hours % 12 || 12;
+    let hoursUpdated;
+
+    // Convert from Military Time
+    if (hours % 12 == 0) {
+        hoursUpdated = 12;
+    }
+    else {
+        hoursUpdated = hours % 12;
+    }
+
+    // If digit is single then add padding 0 to begenning else keep
+    let minutesUpdated = minutes.toString().padStart(2, '0');
+    let AMorPM;
+
+    // Set AMorPM
+    if (PM == true){
+        AMorPM = 'PM'
+    }
+    else {
+        AMorPM = 'AM'
+    }
+    
+    // If digit is single then add padding 0 to begenning else keep
+    let month = (now.getMonth() + 1).toString().padStart(2, '0');
+    let day = now.getDate().toString().padStart(2, '0');
+    let year = now.getFullYear();
+
+    // Email Message Format 
+    let message = 'Critical Safety Event at ' + hoursUpdated + ':' + minutesUpdated +  ' ' + AMorPM +  ' on ' +  month + '/' + day + '/' + year;
+
+    return message; 
+}
+
+console.log(messageTime());
+
+// Transporter --> Initializes Sender Information
 let transporter = nodemailer.createTransport({
     service: 'gmail', 
     auth: {
@@ -24,30 +86,44 @@ let transporter = nodemailer.createTransport({
     }
 });
 
+// MailOptions --> Sets Sender Email, Reciever Emial, Subject, and text (Empty Till Message is Taken)
 let mailOptions = {
     from: 'seniordesignsebshivjohnlogan@gmail.com', 
     to: 'spatel9603@gmail.com',         
-    subject: 'ALERT: INTERUPT HAS OCURRED',           
+    subject: 'ALERT: RECIEVER FAULTY', 
+    text: ''
+
 };
 
-
+/////////////////////////////////////////////////
+// Function: sendAlertEmail()
+// Description: This function sends a email
+//              first by capturing the message 
+//              and then using the 
+//              transporter.sendMail function
+/////////////////////////////////////////////////
 function sendAlertEmail(){
+    message = messageTime();
+    mailOptions.text = message;
     transporter.sendMail(mailOptions, function (error) {
         if (error) {
             return console.log(error);
         }
+        // Log Sent Email On Terminal
         console.log('Email Sent');
     });
 }
 
+// Database reference to specific spot 'Send_Email'
 const emailReference = ref(database, 'Send_Email');
 
-onValue(valueRef, (snapshot) => {
-    const data = snapshot.val();
-    
-    if (data === true){
-        sendAlertEmail();
+// Check if value is true in database
+onValue(emailReference, (snapshot) => {
+    const data = snapshot.val(); // Get Value form 'Send Email' in Database
 
-        set(emailReference, false);
+    if (data === true){
+        sendAlertEmail(); // Send Email
+
+        set(emailReference, false); // Set Value in Database to False
     }
 });
